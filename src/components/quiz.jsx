24a7365c4410fd6dc2e7b8875781ff08f9f02be4
Oponjous <joe.ikdura@gmail.com>
2024-01-02 21:34:15 +0100

    working on indication the correct and wrong answers by button color

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,25 +1,43 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import QUESTIONS from "../questions";
 import Timer from "./quizTimer";
 import image from "../assets/quiz-complete.png";
 import classes from "./quiz.module.css";
 
 function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = useRef();
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
-  const nextQuestionHandler = useCallback((answer) => {
-    setUserAnswers((prevAnswers) => {
-      return [...prevAnswers, answer];
-    });
-  }, []);
+  const nextQuestionHandler = useCallback(
+    (answer) => {
+      setAnswerState("answered");
+
+      setUserAnswers((prevAnswers) => {
+        return [...prevAnswers, answer];
+      });
+
+      setTimeout(() => {
+        if (answer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const skipAnswerHandler = useCallback(
     () => nextQuestionHandler(null),
     [nextQuestionHandler]
   );
-
-  const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
   if (quizIsOver) {
     return (
@@ -29,8 +47,10 @@ function Quiz() {
       </div>
     );
   }
-  const sortedAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  const shuffledAnswers = sortedAnswers.sort(() => Math.random() - 0.5);
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
 
   return (
     <div className={classes.questionBox}>
@@ -40,17 +60,33 @@ function Quiz() {
         onTimeout={skipAnswerHandler}
       />
       <p className={classes.question}>{QUESTIONS[activeQuestionIndex].text}</p>
+
       <ul className={classes.optionUl}>
-        {shuffledAnswers.map((answer) => (
-          <li className={classes.option} key={answer}>
-            <button
-              onClick={() => nextQuestionHandler(answer)}
-              className={classes.optionBtn}
-            >
-              {answer}
-            </button>
-          </li>
-        ))}
+        {shuffledAnswers.current.map((answer) => {
+          const isSelected = userAnswers[userAnswers.length - 1] === answer;
+          let cssClass = "";
+          if (answerState === "answered" && isSelected) {
+            cssClass = classes.selected;
+          }
+
+          if (answerState === "correct" && isSelected) {
+            cssClass = classes.correct;
+          }
+          if (answerState === "wrong" && isSelected) {
+            cssClass = classes.wrong;
+          }
+
+          return (
+            <li className={classes.option} key={answer}>
+              <button
+                onClick={() => nextQuestionHandler(answer)}
+                className={cssClass}
+              >
+                {answer}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
